3f50ea44aa2bddc0b29b256847e33bf5317608e1
chokwangho <whrhkdghekd@naver.com>
2016-04-26 04:13:26 +0900

    문자치환해주는작업

--- Ocrtest/README.md
+++
@@ -1,4 +0,0 @@
-ocr-tess4j-example
-==================
-
-This will demonstrate extracting text from scanned documents ( pdf, jpg, tiff, bmp, png etc)
--- Ocrtest/src/TesseractExample.java
+++ Ocrtest/src/TesseractExample.java
@@ -10,14 +10,23 @@ public class TesseractExample
         File imageFile = new File("samples/sample.integrate.exp0.tif");
   
 		Tesseract instance = Tesseract.getInstance(); // JNA Interface Mapping
-		instance.setDatapath("C:/Users/Kwangho/workspace/Ocrtest/tessdata");
+		instance.setDatapath("C:/Users/Kwangho/git/OCR/Ocrtest/tessdata");
 		instance.setLanguage("sample");
 		//instance.setLanguage("o");
         // Tesseract1 instance = new Tesseract1(); // JNA Direct Mapping
 		  System.out.println(imageFile.canRead());
         try {
             String result = instance.doOCR(imageFile);
-            System.out.println(result);
+            	System.out.print(result);
+             if (result.indexOf("dx")>=0){ //dx가 있으면 dx를 ]로 치환해라
+            	result= result.replace("dx","]");}
+             
+             if(result.indexOf("^4")>=0){
+            	 result=result.replace("^4","4승");}
+             if(result.indexOf("integrate")>=0){
+            	 result=result.replace("integrate","integrate[");  }
+             //규칙찾기
+             System.out.print(result);
         } catch (TesseractException e) {
             System.err.println(e.getMessage());
         }
